27196b9af067adbdcbb237e21d03c1edc7d1c4f7
Sriram Neelamegham Lab Codes <49697937+neel-lab@users.noreply.github.com>
2020-07-09 16:50:21 -0400

    Add files via upload

--- cGlycoTest.m
+++ cGlycoTest.m
@@ -1,63 +1,109 @@
+%% Define the root path of the cGlyco
+rootdir = 'D:\Work\cGlyco(new)';
+
+%% Other dir
+Bcelldir      = fullfile(rootdir,'Testcase\Bcell');
+Monocytesdir  = fullfile(rootdir,'Testcase\Monocytes');
+CompDir       = fullfile(rootdir,'Testcase\BcellMonocytes');
+colormapdir   = fullfile(rootdir,'func');
 %% Step 1 test case - Peak list generation
 % B cell
-loadpath = 'C:\Users\GoEason\Desktop\Paper&dissertation\P0_GDAT\cGlyco\Testcase\Bcell';
-[MSdata,fitPara]=peakList('Bcell_Human','msrawdatapathstr',loadpath);
+[MSdata,fitPara]=peakList('Bcell_Human','dir',Bcelldir);
 %% Step 2 test case - database building 
 % N-linked glycan
 antennalib = {'';...                      % define all possible form of antennas here 1
-    '{n}';...                             % 2
-    '{n{h_b4}}';...                       % 3
-    '{n{f_a3}{h_b4}}';...                 % 4
-    '{n{h_b4{s_a3}}}';...                 % 5
-    '{n{f_a3}{h_b4{s_a3}}}';...           % 6
-    '{n{h_b4{n_b3{h_b4}}}}';...           % 7
-    '{n{h_b4{n_b3{f_a3}{h_b4}}}}';...     % 8
-    '{n{h_b4{n_b3{h_b4{s_a3}}}}}';...     % 9
-    '{n{h_b4{n_b3{f_a3}{h_b4{s_a3}}}}}'}; % 10
+    'GlcNAc(b1-?)';...                             % 2
+    'Gal(b1-4)GlcNAc(b1-?)';...                       % 3
+    'Gal(b1-4)[Fuc(a1-3)]GlcNAc(b1-?)';...                 % 4
+    'NeuAc(a2-3)Gal(b1-4)GlcNAc(b1-?)';...                 % 5
+    'NeuAc(a2-3)Gal(b1-4)[Fuc(a1-3)]GlcNAc(b1-?)';...           % 6
+    'Gal(b1-4)GlcNAc(b1-3)Gal(b1-4)GlcNAc(b1-?)';...           % 7
+    'Gal(b1-4)[Fuc(a1-3)]GlcNAc(b1-3)Gal(b1-4)GlcNAc(b1-?)';...     % 8
+    'NeuAc(a2-3)Gal(b1-4)GlcNAc(b1-3)Gal(b1-4)GlcNAc(b1-?)';...     % 9
+    'NeuAc(a2-3)Gal(b1-4)[Fuc(a1-3)]GlcNAc(b1-3)Gal(b1-4)GlcNAc(b1-?)'}; % 10
 
-coreStruct  ='core1';
-BranchType  ='Bi';
+coreStruct  ='core4';
+BranchType  ='tetra';
 withLinkage = 1;
-storepath   = 'C:\Users\GoEason\Desktop\Paper&dissertation\P0_GDAT\cGlyco\Testcase\';
+outputdir   = Bcelldir;
 highmannose = 1;
 GlyType     = 'N-';
 
-[glycanDB,outputname,LibSize] = genGlyLib(antennalib,coreStruct,withLinkage,storepath,GlyType,BranchType,highmannose);
+[glycanDB,outputname,LibSize] = genGlyLib(antennalib,coreStruct,withLinkage,outputdir,GlyType,BranchType,highmannose);
 
 % O-linked glycan
 antennalib = {'';...                        % define all possible form of antennas here 1
-    '{s}';...                               % 2
-    '{n{h_b4}}';...                         % 3
-    '{n{f_a3}{h_b4}}';...                   % 4
-    '{n{h_b4{s_a3}}}';...                   % 5
-    '{n{f_a3}{h_b4{s_a3}}}';...             % 6
-    '{n{h_b4{n_b3{h_b4}}}}';...             % 7
-    '{n{h_b4{n_b3{f_a3}{h_b4}}}}';...       % 8
-    '{n{h_b4{n_b3{h_b4{s_a3}}}}}';...       % 9
-    '{n{h_b4{n_b3{f_a3}{h_b4{s_a3}}}}}'};   % 10
+    'NeuAc(a2-?)';...                               % 2
+    'Gal(b1-4)GlcNAc(b1-?)';...                         % 3
+    'Gal(b1-4)[Fuc(a1-3)]GlcNAc(b1-?)';...                   % 4
+    'NeuAc(a2-3)Gal(b1-4)GlcNAc(b1-?';...                   % 5
+    'NeuAc(a2-3)Gal(b1-4)[Fuc(a1-3)]GlcNAc(b1-?)';...             % 6
+    'Gal(b1-4)GlcNAc(b1-3)Gal(b1-4)GlcNAc(b1-?)';...             % 7
+    'Gal(b1-4)[Fuc(a1-3)]GlcNAc(b1-3)Gal(b1-4)GlcNAc(b1-?)';...       % 8
+    'NeuAc(a2-3)Gal(b1-4)GlcNAc(b1-3)Gal(b1-4)GlcNAc(b1-?)';...       % 9
+    'NeuAc(a2-3)Gal(b1-4)[Fuc(a1-3)]GlcNAc(b1-3)Gal(b1-4)GlcNAc(b1-?)'};   % 10
 
 coreStruct  = 'core1';
 withLinkage = 1;
-storepath   = 'C:\Users\GoEason\Desktop\Paper&dissertation\P0_GDAT\cGlyco\Testcase\';
+outputdir   = Bcelldir;
 GlyType     = 'O-';
 
-[glycanDB,outputname,LibSize] = genGlyLib(antennalib,coreStruct,withLinkage,storepath,GlyType);
+[glycanDB,outputname,LibSize] = genGlyLib(antennalib,coreStruct,withLinkage,outputdir,GlyType);
 
 %% Step 3 test case - MS abundance
 % B cell
-MSDataFileName         = 'BcellMSdata';
-MSDataLoadpath         = 'C:\Users\GoEason\Desktop\Paper&dissertation\P0_GDAT\cGlyco\Testcase\Bcell';
-glycanDBName           = 'BcellglycanDB';
-glycanDBLoadpath       = 'C:\Users\GoEason\Desktop\Paper&dissertation\P0_GDAT\cGlyco\Testcase\Bcell';
-OverSegmentationFilter = 1.5;
-OutputExcelFileName    = 'B_cell';
-storepath              = 'C:\Users\GoEason\Desktop\Paper&dissertation\P0_GDAT\cGlyco\Testcase\';
-[newglycanDB,outputfilename] = glycanAbundance(MSDataFileName,glycanDBName,MSDataLoadpath,glycanDBLoadpath,OverSegmentationFilter,OutputExcelFileName,storepath);
+MSDataFileName  = 'Bcell_Human';
+MSdir           = Bcelldir;
+glycanDBName    = 'BcellglycanDB';
+glycnDBdir      = Bcelldir;
+OverSegFilter   = 1.5;
+outputExcel     = 'B_cell';
+outputdir       = Bcelldir;
+[newglycanDB,outputfilename] = glycanAbundance(MSDataFileName,glycanDBName,MSdir,glycnDBdir,OverSegFilter,outputExcel,outputdir);
 
 %% Step 4 test case - MS analysis
+% listOfStruct
+listOfStruct = {'LeX','sialylLeX','LacNAc','Sia_LacNAc','Bisecting','coreFuc'};
 % B cell
-MSfilename = 'Bcell';
-GlycanDB   = 'Bcell_HumanglycanDB';
-loadpath   = 'C:\Users\GoEason\Desktop\Paper&dissertation\P0_GDAT\cGlyco\Testcase\Bcell';
-storepath  = 'C:\Users\GoEason\Desktop\Paper&dissertation\P0_GDAT\cGlyco\Testcase';
-newglycanDB = classification(MSfilename,GlycanDB,loadpath,storepath);
+MSfilename  = 'Bcell';
+glycanDB    = 'Bcell_HumanglycanDB';
+glycanDBdir = Bcelldir;
+outputdir   = Bcelldir;
+newglycanDB = classification(MSfilename,glycanDB,glycanDBdir,outputdir,listOfStruct);
+
+%% Comparison analysis
+% listOfStruct
+listOfStruct = {'LeX','sialylLeX','LacNAc','Sia_LacNAc','Bisecting','coreFuc'};
+% Bcell
+glycanDB = fullfile(Bcelldir,'Bcell_HumanglycanDB.mat');
+load(glycanDB,'newglycanDB');
+ResidueResult = MSResidueAnalysisGNAT('Bcell',newglycanDB);
+% Save results to BcellMonocytes folder
+storedir = fullfile(CompDir,'BcellResidue.mat');
+save(storedir,'ResidueResult');
+StructResult  = MSStructAnalysisGNAT('Bcell',newglycanDB,listOfStruct);
+% Save results to BcellMonocytes folder
+storedir = fullfile(CompDir,'BcellStruct.mat');
+save(storedir,'StructResult');
+% Monocytes
+glycanDB = fullfile(Monocytesdir,'Monocytes_HumanglycanDB.mat');
+load(glycanDB,'newglycanDB');
+ResidueResult = MSResidueAnalysisGNAT('Monocytes',newglycanDB);
+% Save results to BcellMonocytes folder
+storedir = fullfile(CompDir,'MonocytesResidue.mat');
+save(storedir,'ResidueResult');
+StructResult  = MSStructAnalysisGNAT('Monocytes',newglycanDB,listOfStruct);
+% Save results to BcellMonocytes folder
+storedir = fullfile(CompDir,'MonocytesStruct.mat');
+save(storedir,'StructResult');
+% Comparison
+ResidueMSFileList = {'BcellResidue','MonocytesResidue'};
+StructMSFileList  = {'BcellStruct','MonocytesStruct'};
+outputfilename    = 'Bcell_Monocytes';
+[ResidueTypeExist,StructTypeExist] = Plotheatmap(ResidueMSFileList,StructMSFileList,outputfilename,CompDir,colormapdir,CompDir);
+% Structure heatmap
+glycanDB1  = 'Bcell_HumanglycanDB';
+glycanDB2  = 'Monocytes_HumanglycanDB';
+loadpath   = CompDir;
+storedir  = CompDir;
+PlotheatmapStruct(glycanDB1,glycanDB2,loadpath,colormapdir,storedir)
